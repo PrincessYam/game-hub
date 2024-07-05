e220ca63fb73564599eb24726ae30272c0d0b1ae
PrincessYam <yammjin023@gmail.com>
2024-07-05 16:41:09 +0800

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import useGenre from "../hooks/useGenre";
+
+export const GenreList = () => {
+  const { genres } = useGenre();
+
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
